99f6a1643646a979166f54d8e0f246f8819b9311
GNerrell <106912430+GNerrell@users.noreply.github.com>
2023-09-07 23:07:03 +0300

    Add files via upload

---
+++ 08-03-brovkin-insert_data.sql
@@ -0,0 +1,24 @@
+INSERT INTO PAWNS VALUES (1,1,1,for several months,50000,10, 11.12.19, 10.04.20);
+INSERT INTO PAWNS VALUES (2,1,2,get it later,30000,10, 10.03.20, 19.09.20);
+INSERT INTO PAWNS VALUES (3,2,3,realiable stay,40000,10, 20.12.19, 06.08.20);
+INSERT INTO PAWNS VALUES (4,3,4,no description,250000,10, 17.12.19, );
+INSERT INTO PAWNS VALUES (5,3,5,take care of car,120000,10, 05.03.22, 15.09.22);
+INSERT INTO PAWNS VALUES (6,4,6,no description,35000,10, 08.12.21, );
+INSERT INTO PAWNS VALUES (7,4,2,default holding,133000,10, 12.12.22, );
+INSERT INTO PAWNS VALUES (8,1,4,care holding,89000,10, 02.12.19, 03.01.20);
+INSERT INTO PAWNS VALUES (9,2,3,i dont know,153000,10, 22.03.20, 10.11.20);
+INSERT INTO PAWNS VALUES (10,2,1,some description,211000,10, 13.12.21, 30.06.22);
+INSERT INTO PAWNS VALUES (11,3,5,some description,199000,10, 18.03.22, 02.12.22);
+
+INSERT INTO CLIENTS VALUES (1,Brovkin,Artem,Alekseevich,777443,3232,some comment);
+INSERT INTO CLIENTS VALUES (2,Petrov,Ivan,Alekseevich,123345,6513,another comment);
+INSERT INTO CLIENTS VALUES (3,Sidorov,Artem,Olegovich,558065,6116,just comment);
+INSERT INTO CLIENTS VALUES (4,Ivanov,Ivan,Alekseevich,738497,3232,simple comment);
+INSERT INTO CLIENTS VALUES (5,Ludovsky,Sergey,Sergeevich,678434,9109,comment);
+INSERT INTO CLIENTS VALUES (6,Golinin,Ivan,Alekseevich,986738,8118,no comment);
+
+INSERT INTO CATEGORIES_OF_PAWNS (1,jewelery,precious metals);
+INSERT INTO CATEGORIES_OF_PAWNS (2,technic,telephone, TV etc);
+INSERT INTO CATEGORIES_OF_PAWNS (3,fur products,fur coats, hats, vests);
+INSERT INTO CATEGORIES_OF_PAWNS (4,car,goes to the parking lot);
+
